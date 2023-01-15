e361d4cb076ac4178bc0c67900dde4e66c3af660
Stephanie Chary <111113294+Ste-Cy@users.noreply.github.com>
2023-01-15 16:12:08 +0100

    fix UnhandledPromiseRejectionWarning

--- server.js
+++ server.js
@@ -4,16 +4,11 @@ const config = require('./src/config');
 const app = require('./src/app');
 const sequelize = require('./src/config/sequelize');
 
-sequelize
-  .authenticate()
+sequelize.sync()
   .then(() => {
-    sequelize.sync()
-      .then(() => {
-        app.listen(config.PORT, () => {
-          if (process.env.NODE_ENV === 'development') {
-            console.log(`Server listening at ${config.HOST}`);
-          }
-        });
-      })
-      .catch((error) => { throw new Error(error); });
-  }).catch((error) => { throw new Error(error); });
+    app.listen(config.PORT, () => {
+      if (process.env.NODE_ENV === 'development') {
+        console.log(`Server listening at ${config.HOST}`);
+      }
+    });
+  }).catch((error) => { console.log(error); });
